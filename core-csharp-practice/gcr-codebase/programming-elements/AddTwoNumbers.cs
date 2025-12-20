207a8c212ef58fe1c3a79365db0e5de2aea2539b
ujjawal <ujjawalvermauv12@gmail.com>
2025-12-20 11:12:01 +0530

    [Ujjawal Verma] add AddTwoNumbers

---
+++ core-csharp-practice/gcr-codebase/programming-elements/AddTwoNumbers.cs
@@ -0,0 +1,11 @@
+using System;
+
+class AddTwoNumbers
+{
+    static void Main(string[] args){
+        int n1=2;
+        int n2=4;
+        int sum=n1+n2;
+        Console.WriteLine(sum);
+    }
+}
