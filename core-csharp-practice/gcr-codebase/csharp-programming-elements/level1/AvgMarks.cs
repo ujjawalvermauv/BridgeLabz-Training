e3bd0d7298671bd205c2a126e8e5786445f455f4
ujjawal <ujjawalvermauv12@gmail.com>
2025-12-20 14:43:48 +0530

    [Ujjawal Verma] AvgMarks

---
+++ core-csharp-practice/gcr-codebase/csharp-programming-elements/level1/AvgMarks.cs
@@ -0,0 +1,13 @@
+using System;
+
+class AvgMarks{
+    static void Main(){
+        string name="sam";
+        int p=95;
+        int c=96;
+        int m=94;
+
+        double avg=p+m+c/3.0;
+        Console.WriteLine(name+"your avg marks is" +avg);
+    }
+}
