b431e5abf78682b4a3de1e2f012e47fc8afb4ffa
ujjawal <ujjawalvermauv12@gmail.com>
2025-12-23 21:25:24 +0530

    [UjjawalVerma]FactorsOfNumbers

---
+++ core-csharp-practice/gcr-codebase/csharp-control-flows/level2/FactorsOfNumbers.cs
@@ -0,0 +1,17 @@
+using System;
+
+class FactorsOfNumbers
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
