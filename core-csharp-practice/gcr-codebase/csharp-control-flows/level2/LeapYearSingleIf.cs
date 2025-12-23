e52d5c9e8f5f5bc9ba4e63c6d56f4ece428b7338
ujjawal <ujjawalvermauv12@gmail.com>
2025-12-23 20:32:04 +0530

    [UjjawalVerma]LeapYearSingleIf

---
+++ core-csharp-practice/gcr-codebase/csharp-control-flows/level2/LeapYearSingleIf.cs
@@ -0,0 +1,18 @@
+using System;
+
+class LeapYearSingleIf
+{
+    static void Main()
+    {
+        int year = int.Parse(Console.ReadLine());
+
+        if (year >= 1582 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)))
+        {
+            Console.WriteLine("Year is a Leap Year");
+        }
+        else
+        {
+            Console.WriteLine("Year is not a Leap Year");
+        }
+    }
+}
