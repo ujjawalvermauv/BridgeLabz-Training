2b8efb400be63052b4fb1a73a3e964bc37530bd9
ujjawal <ujjawalvermauv12@gmail.com>
2025-12-24 11:55:16 +0530

    [UjjawalVerma]MeanHeight

---
+++ core-csharp-practice/gcr-codebase/c# arrays/level1/MeanHeight.cs
@@ -0,0 +1,15 @@
+using System;
+
+class MeanHeight{
+    static void Main(){
+        double [] heights = new double[11];
+        double sum =0;
+        for(int i =0;i<heights.Length;i++){
+            Console.WriteLine("Enter height of player " + (i + 1) + ": ");
+            heights[i] = double.Parse(Console.ReadLine());
+            sum += heights[i];
+        }
+        double mean = sum / heights.Length;
+        Console.WriteLine("Mean height of team: " + mean);
+    }
+}
