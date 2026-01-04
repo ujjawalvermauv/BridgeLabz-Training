bf8fb478077f474b70ccf28aca6d16ec4b751a1c
ujjawal <ujjawalvermauv12@gmail.com>
2026-01-04 23:05:09 +0530

    [Ujjawal Verma]EmployeeManagementSystem

---
+++ oops-csharp-practice/gcr-codebase/cshrap-Inheritance/EmployeeManagementSystem.cs
@@ -0,0 +1,79 @@
+using System;
+class Employee
+{
+    // Attributes
+    public string Name;
+    public int ID;
+    public double salary;
+    public Employee(string name, int id, double salary) //constructor
+    {
+        Name = name;
+        ID = id;
+        this.salary = salary;
+    }
+    public virtual void DisplayDetails() //virtual method
+    {
+        Console.WriteLine("Employee Name: " + Name);
+        Console.WriteLine("Employee ID: " + ID);
+        Console.WriteLine("Employee Salary: " + salary);
+    }
+
+}
+class Manager : Employee
+{
+    public int teamSize;
+    public Manager(string name, int id, double salary, int teamSize) : base(name, id, salary)
+    {
+        this.teamSize = teamSize;
+    }
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine("Team Size: " + teamSize);
+    }
+
+
+
+}
+class Developer : Employee
+{
+    public string programmingLanguage;
+    public Developer(string name, int id, double salary, string programmingLanguage) : base(name, id, salary)
+    {
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine("Programming Language: " + ProgrammingLanguage);
+    }
+}
+class Intern : Employee
+{
+    public string InternshipDuration;
+    public Intern(string name, int id, double salary, string InternshipDuration) : base(name, id, salary)
+    {
+        this.InternshipDuration = InternshipDuration;
+    }
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine("Internship Duration: " + InternshipDuration);
+    }
+}
+class EmployeeManagementSystem
+{
+    public static void Main()
+    {
+        Employee emp1 = new Manager("Amar", 101, 90000, 10);
+        Employee emp2 = new Developer("Bobby", 102, 80000, "C#");
+        Employee emp3 = new Intern("Gupta", 103, 30000, "6 months");
+
+        emp1.DisplayDetails();
+        Console.WriteLine();
+        emp2.DisplayDetails();
+        Console.WriteLine();
+        emp3.DisplayDetails();
+    }
+}
