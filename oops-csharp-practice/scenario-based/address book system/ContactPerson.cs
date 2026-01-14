dbc559832a11fd38a698b302d100f975ef402ef6
ujjawal <ujjawalvermauv12@gmail.com>
2026-01-14 15:09:44 +0530

    [Ujjawal Verma]Address Book uc1

---
+++ oops-csharp-practice/scenario-based/address book system/ContactPerson.cs
@@ -0,0 +1,73 @@
+using System;
+
+public class ContactPerson
+{
+    //uc1
+    private string FirstName;
+    private string SecondName;
+    private string Address;
+    private string City;
+    private string State;
+    private string PhoneNumber;
+    private string Email;
+
+    public void SetFirstName(string FirstName) 
+    {
+        this.FirstName=FirstName;
+    }
+    public GetFirstName(){
+        return FirstName;
+        
+    }
+    public void SetLastName(string Lastame) 
+    {
+        this.LastNameName=Lastame;
+    }
+    public GetLastName(){
+        return LastName;
+        
+    }
+    public void SetAddress(string Adress) 
+    {
+        this.Address=Address;
+    }
+    public GetAddress(){
+        return Address;
+        
+    }
+    public void SetCity(string City) 
+    {
+        this.City=City;
+    }
+    public GetCity(){
+        return City;
+        
+    }
+    public void SetState(string State) 
+    {
+        this.State=State;
+    }
+    public GetState(){
+        return State;
+        
+    }
+    public void SetPhoneNumber(string PhoneNumber) 
+    {
+        this.PhoneNumber=PhoneNumber;
+    }
+    public GetPhoneNumber(){
+        return PhoneNumber;
+        
+    }
+    public void SetEmail(string Email)
+    {
+        this.Email=Email;
+        
+    }
+    public GetEmail()
+    {
+        return Email;
+    }
+    
+
+}
