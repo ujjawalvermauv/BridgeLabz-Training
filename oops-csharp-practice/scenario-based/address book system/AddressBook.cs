34233ee309f0f18705e38ba5373f3d57a7579056
ujjawal <ujjawalvermauv12@gmail.com>
2026-01-15 11:53:33 +0530

    [Ujjawal Verma]uc2

--- oops-csharp-practice/scenario-based/address book system/AddressBook.cs
+++ oops-csharp-practice/scenario-based/address book system/AddressBook.cs
@@ -8,4 +8,12 @@ public void AddContact(Contact contact)
 Console.WriteLine("Contact Added:");
 Console.WriteLine(contact);
 }
+public void EditContact(string firstName,Contact updated)
+    {
+        if(firstName == updated.FirstName)
+        {
+            System.Console.WriteLine("Contact Updated");
+            System.Console.WriteLine(updated);
+        }
+    }
 }
